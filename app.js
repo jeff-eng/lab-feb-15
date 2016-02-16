2ef59d0704a78274d5a2ac18f9dac95f2c83f525
Jeff Eng <jeff@jeffeng.me>
2016-02-15 19:01:28 -0800

    multiply function created and tested in console

--- app.js
+++ app.js
@@ -17,3 +17,8 @@ function sum(num1, num2) {
   var result = num1 + num2;
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + result + '.');
 }
+
+function multiply(num1, num2) {
+  var result = num1 * num2;
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + result + '.');
+}
